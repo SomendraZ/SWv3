98f29744266496c9344e745a6aa35f17ed025d41
SomendraZ <somendra.8.1.2003@gmail.com>
2025-01-19 03:09:11 +0530

    Add level progression and alien laser styling to Space Invaders game

--- spaceInvaders/app.js
+++ spaceInvaders/app.js
@@ -7,6 +7,8 @@ let invadersId;
 let goingRight = true;
 let alienInvadersRemoved = [];
 let result = 0;
+let currentLevel = 1;
+let moveInterval = 250;
 
 for (let i = 0; i < 225; i++) {
   const square = document.createElement("div");
@@ -15,10 +17,22 @@ for (let i = 0; i < 225; i++) {
 
 const squares = Array.from(document.querySelectorAll(".grid div"));
 
-const alienInvaders = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31,
-  32, 33, 34, 35, 36, 37, 38, 39,
+const levels = [
+  [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39,
+  ],
+  [
+    0, 1, 2, 3, 4, 15, 16, 17, 18, 19, 30, 31, 32, 33, 34, 45, 46, 47, 48, 49,
+    60, 61, 62, 63, 64,
+  ],
+  [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21, 22, 23, 24, 32,
+    33, 34, 35, 36, 37, 38, 48, 49, 50, 51, 52, 64, 65, 66, 80,
+  ],
 ];
+
+let alienInvaders = [...levels[0]];
 
 function draw() {
   for (let i = 0; i < alienInvaders.length; i++) {
@@ -27,8 +41,6 @@ function draw() {
     }
   }
 }
-
-draw();
 
 function remove() {
   for (let i = 0; i < alienInvaders.length; i++) {
@@ -53,22 +65,22 @@ function moveShooter(e) {
 document.addEventListener("keydown", moveShooter);
 
 function moveInvaders() {
-  const leftEdge = alienInvaders[0] % 15 === 0;
-  const rightEdge = alienInvaders[alienInvaders.length - 1] % 15 === 14;
+  const leftEdge = alienInvaders.some((invader) => invader % 15 === 0);
+  const rightEdge = alienInvaders.some((invader) => invader % 15 === 14);
   remove();
 
   if (rightEdge && goingRight) {
     for (let i = 0; i < alienInvaders.length; i++) {
       alienInvaders[i] += 15 + 1;
-      direction = -1;
-      goingRight = false;
     }
+    direction = -1;
+    goingRight = false;
   } else if (leftEdge && !goingRight) {
     for (let i = 0; i < alienInvaders.length; i++) {
       alienInvaders[i] += 15 - 1;
-      direction = 1;
-      goingRight = true;
     }
+    direction = 1;
+    goingRight = true;
   }
 
   for (let i = 0; i < alienInvaders.length; i++) {
@@ -85,22 +97,37 @@ function moveInvaders() {
   }
 
   for (let i = 0; i < alienInvaders.length; i++) {
-    if (alienInvaders[i] >= 224) {
+    if (alienInvaders[i] >= squares.length) {
       scoreDisplay.innerHTML = "Game Over";
       document.removeEventListener("keydown", moveShooter);
       document.removeEventListener("keyup", shoot);
       clearInterval(invadersId);
     }
   }
+
   if (alienInvadersRemoved.length === alienInvaders.length) {
-    scoreDisplay.innerHTML = "You Win";
-    document.removeEventListener("keydown", moveShooter);
-    document.removeEventListener("keyup", shoot);
-    clearInterval(invadersId);
+    if (currentLevel < levels.length) {
+      nextLevel();
+    } else {
+      scoreDisplay.innerHTML = "You Win!";
+      document.removeEventListener("keydown", moveShooter);
+      document.removeEventListener("keyup", shoot);
+      clearInterval(invadersId);
+    }
   }
 }
 
-invadersId = setInterval(moveInvaders, 250);
+function nextLevel() {
+  currentLevel++;
+  alienInvaders = [...levels[currentLevel - 1]];
+  alienInvadersRemoved = [];
+  moveInterval -= 50;
+  clearInterval(invadersId);
+  invadersId = setInterval(moveInvaders, moveInterval);
+  draw();
+}
+
+invadersId = setInterval(moveInvaders, moveInterval);
 
 function shoot(e) {
   let laserId;
